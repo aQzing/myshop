40adede02e3999e54ac43f696f2fcfde453e0a56
qzing <1107382263@qq.com>
2019-05-23 00:51:07 +0800

    init

---
+++ myshop-commons-mapper/src/main/java/cn/itqzing/myshop/commons/mapper/TbItemCatMapper.java
@@ -0,0 +1,7 @@
+package cn.itqzing.myshop.commons.mapper;
+
+import cn.itqzing.myshop.commons.domain.TbItemCat;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbItemCatMapper extends MyMapper<TbItemCat> {
+}
---
+++ myshop-commons-mapper/src/main/java/cn/itqzing/myshop/commons/mapper/TbUserMapper.java
@@ -0,0 +1,7 @@
+package cn.itqzing.myshop.commons.mapper;
+
+import cn.itqzing.myshop.commons.domain.TbUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbUserMapper extends MyMapper<TbUser> {
+}
